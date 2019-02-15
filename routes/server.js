6263c2e328f704247196626feacc095531b59e47
dwyfrequency <jdwy215@gmail.com>
2019-02-15 09:19:34 -0500

    changing structure of directory tree

--- routes/server.js
+++ routes/server.js
@@ -6,6 +6,12 @@ const app = express();
 // DB config
 const db = require("./config/keys").mongoURI;
 
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const port = process.env.Port || 5000;
