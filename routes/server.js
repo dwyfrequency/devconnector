fdf4a46b0b37e9d76dbfe9ddfbaf8768ab6f2227
dwyfrequency <jdwy215@gmail.com>
2019-04-05 12:34:52 -0400

    adding updates for routes

--- routes/server.js
+++ routes/server.js
@@ -1,27 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
 
-const users = require("./api/users");
-const profile = require("./api/profile");
-const posts = require("./api/posts");
+const users = require('./api/users');
+const profile = require('./api/profile');
+const posts = require('./api/posts');
 
 const app = express();
 
 // DB config
-const db = require("../config/keys").mongoURI;
+const db = require('../config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get('/', (req, res) => res.send('Hello World!'));
 
 // Use Routes
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
 const port = process.env.Port || 5000;
 
